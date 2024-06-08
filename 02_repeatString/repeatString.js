74ae6e6f87799d031912f7186388dac1c5bbed30
[gareth hines] <gareth_hines@hotmail.com>
2024-06-08 09:56:18 +0100

    Complete Exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeats) {
+    let sentence = string; 
+    for (let i = 1 ; i < repeats ; i++ ) {
+        sentence += string;
+    }
+    return sentence;
 };
 
 // Do not edit below this line
