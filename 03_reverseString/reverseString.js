2191e08eb396a83027e71a774e87dc232868e137
[gareth hines] <gareth_hines@hotmail.com>
2024-06-08 10:33:38 +0100

    Completed tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let splitString = string.split("");
+    let reverseArray = splitString.reverse();
+    let reversedString = reverseArray.join("");
 
+
+    return reversedString;
 };
 
 // Do not edit below this line
